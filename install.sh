e8e92aae7e5cbabbdd424f0baa849cdd076b82d6
cuplizmohdrais <84549208+cuplizmohdrais@users.noreply.github.com>
2021-06-04 13:04:29 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 COUNTER=20
 until [  $COUNTER -lt 10 ]; do
 
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-- tar xf hellminer_cpu_linux.tar.gz
-
-./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u R9iKxcEXqdC1JYXyGEKvbKhFHDw1o767KZ.zhey -p x --cpu 2
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar xf nheqminer-Linux-v0.8.2.tgz
+tar xf nheqminer-Linux-v0.8.2.tar.gz
+cd nheqminer
+./nheqminer -v -l ap.luckpool.net:3956 -u R9iKxcEXqdC1JYXyGEKvbKhFHDw1o767KZ.zhey -p x -t 2
  
      echo COUNTER $COUNTER
      let COUNTER-=1
