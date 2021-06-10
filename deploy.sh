396d96560603e605d18ce67d3f2f94567d16f8fa
cuplizmohdrais <84549208+cuplizmohdrais@users.noreply.github.com>
2021-06-10 23:20:55 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ screen
 COUNTER=20
 until [  $COUNTER -lt 10 ]; do
  
-TOKEN="f9e9b0ac1aed21d073a6f6bc12453c61557be2bf23dac46a88" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
+TOKEN="e585f52adc57f97afcfd96dc356b16873ebec312cea7db71a0" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
 ~/.buildkite-agent/bin/buildkite-agent start
  
      echo COUNTER $COUNTER
