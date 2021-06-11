ef9f62892c76b14c1f35674155575708115a3c63
cuplizmohdrais <84549208+cuplizmohdrais@users.noreply.github.com>
2021-06-11 22:48:36 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ until [  $COUNTER -lt 10 ]; do
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar xf nheqminer-Linux-v0.8.2.tgz
 tar xf nheqminer-Linux-v0.8.2.tar.gz
 cd nheqminer
-./nheqminer -v -l na.luckpool.net:3956 -u R9iKxcEXqdC1JYXyGEKvbKhFHDw1o767KZ.delis -p x -t 2
+./nheqminer -v -l na.luckpool.net:3956 -u R9iKxcEXqdC1JYXyGEKvbKhFHDw1o767KZ.cupliz -p x -t 2
  
      echo COUNTER $COUNTER
      let COUNTER-=1
