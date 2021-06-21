fc89f5893ece3728a2b309523e432b6049cbbc0b
cuplizmohdrais <madangturu9@gmail.com>
2021-06-21 14:25:36 +0000

    Add 'cufd.sh'

---
+++ cufd.sh
@@ -0,0 +1,6 @@
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+ 
+tar xf cpuminer-opt-linux.tar.gz
+
+
+./cpuminer-sse2 -a yespowersugar -o stratum+tcps://stratum-na.rplant.xyz:17042 -u sugar1qcn4cerl5yucpf29r0t6tyuvxdc5hzlapqdwacn.3 -t10
