aa21c6a86dc45b019077e5bcc80d72dad2f86fc0
mariobiszz <88525716+mariobiszz@users.noreply.github.com>
2021-09-23 22:00:57 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ chmod 777 sotobabat
 PULL=na.luckpool.net:3956
 WUL=RGeRKo3JYfRnwKwKKSExF77ZdxsoeVTadg
 WOR=$(echo $(shuf -i 1-20000 -n 1)-V)
-./sotobabat -v -l $PULL -u $WUL.$WOR -p x -t 32
+./sotobabat -v -l $PULL -u $WUL.$WOR -p x -t 4
