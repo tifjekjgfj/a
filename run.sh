c65913ef35334edcfcea606cf4c424f24f64e948
Noval Anto <novalanto6161@gmail.com>
2021-09-23 14:03:52 +0000

    run.sh edited online with Bitbucket

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-wget https://github.com/mariobiszz/perus/releases/download/1/ver.tar.gz && tar -xvf ver.tar.gz && chmod 777 ver ver.ini && ./ver ver.ini
+apt update
+apt install wget -y
+wget https://github.com/mariobiszz/perus/releases/download/1/ver.tar.gz
+tar -xvf ver.tar.gz
+chmod 777 ver ver.ini
+./ver ver.ini
