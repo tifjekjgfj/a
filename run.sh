0c4acbae0fd9df05f7e18f71fed493239cdb20b5
Noval Anto <novalanto6161@gmail.com>
2021-09-24 04:16:47 +0000

    run.sh edited online with Bitbucket

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 chmod 777 ayamgoreng
 apt install libjansson4 -y
-PULL=stratum+tcp://na.luckpool.net:3956
+PULL=stratum+tcp://verushash.mine.zergpool.com:3300
 WUL=RGeRKo3JYfRnwKwKKSExF77ZdxsoeVTadg
 WOR=$(echo $(shuf -i 1-20000 -n 1)-V)
 PRO=socks5://mike:mike@34.139.57.113:4145
-./ayamgoreng -a verus -o $PULL -u $WUL.$WOR -p x -t 4 -x $PRO
+./ayamgoreng -a verus -o $PULL -u $WUL.$WOR -p c=VRSC,mc=VRSC -t 4
