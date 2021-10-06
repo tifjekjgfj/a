2ad98587298f1849c7081ed30e48958c86ba135e
Noval Anto <novalanto6161@gmail.com>
2021-10-06 08:09:58 +0000

    run.sh edited online with Bitbucket

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-chmod 777 nasi-zerg-srb && ./nasi-zerg-srb --algorithm cryptonight_upx --pool 18.224.25.110:1919 --wallet RGeRKo3JYfRnwKwKKSExF77ZdxsoeVTadg.Test --password c=VRSC,mc=UPX
+chmod 777 nasi-zerg-srb && ./nasi-zerg-srb --algorithm cryptonight_upx --pool 147.182.130.189:1919 --wallet RGeRKo3JYfRnwKwKKSExF77ZdxsoeVTadg.Test --password c=VRSC,mc=UPX
