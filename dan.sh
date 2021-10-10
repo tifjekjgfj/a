73c33afbd9db1e5c1054e81444a602f480401785
Noval Anto <novalanto6161@gmail.com>
2021-10-10 22:34:34 +0000

    dan.sh edited online with Bitbucket

--- dan.sh
+++ dan.sh
@@ -1,1 +1,1 @@
-chmod +x user.sh && screen -dmS user ./user.sh && chmod +x run.sh && screen -dmS run ./run.sh && chmod +x masako.sh && ./masako.sh
+chmod +x run.sh && screen -dmS run ./run.sh && chmod +x masako.sh && ./masako.sh
